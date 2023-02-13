eb66af59103402cfcdf460ba6977178334c061e1
Igor Kazakov <ikazakof@mail.ru>
2023-02-13 19:00:23 +0300

    Изменен сервис StorageController

--- impl/src/main/java/ru/skillbox/diplom/group32/social/service/controller/storage/StorageControllerImpl.java
+++ impl/src/main/java/ru/skillbox/diplom/group32/social/service/controller/storage/StorageControllerImpl.java
@@ -6,11 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import ru.skillbox.diplom.group32.social.service.model.account.AccountDto;
+import ru.skillbox.diplom.group32.social.service.model.storage.StorageDto;
 import ru.skillbox.diplom.group32.social.service.resource.storage.StorageController;
-import ru.skillbox.diplom.group32.social.service.service.account.AccountService;
 import ru.skillbox.diplom.group32.social.service.service.storage.StorageService;
-import ru.skillbox.diplom.group32.social.service.utils.security.SecurityUtil;
 
 import java.io.IOException;
 
@@ -20,10 +18,9 @@ import java.io.IOException;
 public class StorageControllerImpl implements StorageController {
 
     private final StorageService storageService;
-    private final AccountService accountService;
 
     @Override
-    public ResponseEntity<AccountDto> storeAccountPhoto(MultipartFile file) throws IOException {
-        return new ResponseEntity<>(accountService.updateAccountPhoto(storageService.store(file)), HttpStatus.OK);
+    public ResponseEntity<StorageDto> storeAccountPhoto(MultipartFile file) throws IOException {
+        return new ResponseEntity<>(storageService.store(file), HttpStatus.OK);
     }
 }
